219fb1e755b610f31cd920a8fa74ee77f63d5891
Scott Hardgrave <scott.hardgrave@yahoo.com>
2018-11-07 10:06:06 -0600

    Mens & Womens Health done. They may need some tweaking to iron out if some of the values are REALLY neccesary or if some of them actually need to be [Required]

--- ArchpointMvcPHR/Models/MensHealth.cs
+++ ArchpointMvcPHR/Models/MensHealth.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,52 @@ namespace ArchpointMvcPHR.Models
 {
     public class MensHealth
     {
+        public int MensHealthHistoryId { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateLastAnnualPhysical { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateNextAnnualPhysical { get; set; }
+
+        public string FamilyHistoryProstateCancer { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateLastProstateExam { get; set; }
+
+        public string LastProstateExamResults { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateNextProstateExam { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateLastPsa { get; set; }
+
+        public string LastPsaresults { get; set; }
+
+        public int PsaLevelTestNumber { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateNextPsatest { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateLastFecalOccultTest { get; set; }
+
+        public string FecalOccultResults { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateNextFecalOccultTest { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateLastSigmoidColonoscopyExam { get; set; }
+
+        public string SigmoidColonoscopyResults { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateNextSigmoidColonoscopy { get; set; }
+
+        public string MensHealthIssues { get; set; }
+
+        public string Comments { get; set; }
     }
 }
--- ArchpointMvcPHR/Models/WomensHealthHistory.cs
+++ ArchpointMvcPHR/Models/WomensHealthHistory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,38 +18,84 @@ namespace ArchpointMvcPHR.Models
         public int WomensHealthHistoryId { get; set; }
 
         public int MedCareProviderId { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DateLastClinicialBreastExam { get; set; }
+
         public string ResultsLastBreastExam { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DateNextBreastExam { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DateLastMammogram { get; set; }
+
         public string ResultsLastMammogram { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DateNextMammogram { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DateLastPapExam { get; set; }
+
         public string ResultsLastPapExam { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DateNextPapExam { get; set; }
+
         public string WomensDiseaseDisorder { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DateOfDiagnosis { get; set; }
+
         public string WomensDiseaseDisorderStatus { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DateResolved { get; set; }
+
         public string WomensHealthSurgeryProcedure { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DateOfSurgeryProcedure { get; set; }
+
+        /*
         public int MedCareLocationId { get; set; }
+        */
+
         public int AgePeriodsStarted { get; set; }
+
         public string PeriodsRegular { get; set; }
+
         public string PeriodProblems { get; set; }
+
         public string PeriodFlow { get; set; }
+
         public string StillHavingPeriods { get; set; }
+
         public string BirthControlMethod { get; set; }
+
         public string HormoneReplacementTherapy { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime HrtDateStarted { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime HrtDateStopped { get; set; }
+
         public string NumOfPregnancies { get; set; }
+
         public string NumOfLiveBirths { get; set; }
+
         public string NumOfPrematureBirths { get; set; }
+
         public string NumOfStillBirths { get; set; }
+
         public string NumOfMiscarriages { get; set; }
+
         public string NumOfCsections { get; set; }
+
         public string NumOfElectiveAbortions { get; set; }
+
         public string PregnancyComplications { get; set; }
 
     }
